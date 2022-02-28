e33b09b7bbe4432c7c9e648bf73dd254a6b8cc00
billyboy96 <etiennenemes@gmail.com>
2022-02-28 15:26:43 -0500

    Deploy app

--- client/src/Components/SearchBar.js
+++ client/src/Components/SearchBar.js
@@ -1,11 +1,17 @@
-import { useEffect} from "react";
+import { useEffect, useState } from "react";
 
 const SearchBar = (props) => {
   const { date, setDate, setDateTitle, currentValue, fetchWeather } = props;
 
+  const [reset, setReset] = useState(false);
   useEffect(() => {
     document.querySelector('input[name="datesearch"]').value = date;
-  }, [date]);
+
+    if (reset === true) {
+      fetchWeather(currentValue);
+      setReset(false);
+    }
+  }, [date, reset]);
 
   const handleEvent = (e) => {
     setDate(e.target.value);
@@ -17,12 +23,23 @@ const SearchBar = (props) => {
     fetchWeather(currentValue);
   };
 
+  const submitDefault = () => {
+    setDate("yyyy-MM-dd");
+    setDateTitle("in the last 30 days");
+    setReset(true);
+  };
+
   return (
-    <form className="search-bar" onSubmit={(e) => handleSubmit(e)}>
-      <h3>Looking for a specific date?</h3>
-      <input type="date" name="datesearch" onChange={(e) => handleEvent(e)} />
-      <input type="submit" value="Search" className="btn btn-submit" />
-    </form>
+    <div className="search-bar-wrapper">
+      <form className="search-bar" onSubmit={(e) => handleSubmit(e)}>
+        <h3>Looking for a specific date?</h3>
+        <input type="date" name="datesearch" onChange={(e) => handleEvent(e)} />
+        <input type="submit" value="Search" className="btn btn-submit" />
+      </form>
+      <button className="btn btn-submit" onClick={() => submitDefault()}>
+        Last 30 days
+      </button>
+    </div>
   );
 };
 
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const path = require('path');
 require('dotenv').config()
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Api", apiKey: process.env.REACT_APP_API_KEY });
+  res.json({apiKey: process.env.REACT_APP_API_KEY });
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
