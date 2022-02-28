01079477d754f430a6969173405d9b5323a56dae
billyboy96 <etiennenemes@gmail.com>
2022-02-28 15:57:55 -0500

    deploy heroku

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
 app.get("/api", (req, res) => {
   res.json({apiKey: process.env.REACT_APP_API_KEY });
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
